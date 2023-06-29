14847bc4e3733741699e78c1640740f8d8f4d411
Vinh Đỗ xuân <vinhdx.k57@gmail.com>
2023-06-29 14:24:00 +0700

    FIX CRASH - when appearance not support by old react native version

--- src/color/_scheme.ts
+++ src/color/_scheme.ts
@@ -1,81 +1,82 @@
 import _ from "lodash";
-import {Appearance} from "react-native";
+import { Appearance } from "react-native";
 import Colors from "./_color";
 
 type Schemes = {
-  light: {[key: string]: string};
-  dark: {[key: string]: string};
+    light: { [key: string]: string };
+    dark: { [key: string]: string };
 };
 type SchemeType = "default" | "light" | "dark";
 
 export class SchemaClass {
-  [key: string]: any;
+    [key: string]: any;
 
-  schemes: Schemes = {light: {}, dark: {}};
+    schemes: Schemes = { light: {}, dark: {} };
 
-  currentScheme: SchemeType = "default";
+    currentScheme: SchemeType = "default";
 
-  constructor() {
-      Object.assign(this, Colors);
+    constructor() {
+        Object.assign(this, Colors);
 
-      Appearance.addChangeListener(() => {
-          if (this.currentScheme === "default") {
-              Object.assign(
-                  this,
-                  this.schemes[Appearance.getColorScheme() ?? "light"],
-              );
-          }
-      });
-  }
+        Appearance &&
+            Appearance?.addChangeListener(() => {
+                if (this.currentScheme === "default") {
+                    Object.assign(
+                        this,
+                        this.schemes[Appearance?.getColorScheme() ?? "light"]
+                    );
+                }
+            });
+    }
 
-  /**
-   * Load set of schemes for light/dark mode
-   * arguments:
-   * schemes - two sets of map of colors e.g {light: {screen: 'white'}, dark: {screen: 'black'}}
-   */
+    /**
+     * Load set of schemes for light/dark mode
+     * arguments:
+     * schemes - two sets of map of colors e.g {light: {screen: 'white'}, dark: {screen: 'black'}}
+     */
 
-  loadSchemes(schemes: Schemes) {
-      const lightSchemeKeys = Object.keys(schemes.light);
-      const darkSchemeKeys = Object.keys(schemes.dark);
+    loadSchemes(schemes: Schemes) {
+        const lightSchemeKeys = Object.keys(schemes.light);
+        const darkSchemeKeys = Object.keys(schemes.dark);
 
-      const missingKeys = _.xor(lightSchemeKeys, darkSchemeKeys);
-      if (!_.isEmpty(missingKeys)) {
-          console.error(
-              `There is a mismatch in scheme keys: ${missingKeys.join(", ")}`,
-          );
-      }
+        const missingKeys = _.xor(lightSchemeKeys, darkSchemeKeys);
+        if (!_.isEmpty(missingKeys)) {
+            console.error(
+                `There is a mismatch in scheme keys: ${missingKeys.join(", ")}`
+            );
+        }
 
-      this.schemes = schemes;
-      const colorScheme = this.getScheme();
-      Object.assign(this, this.schemes[colorScheme]);
-  }
+        this.schemes = schemes;
+        const colorScheme = this.getScheme();
+        Object.assign(this, this.schemes[colorScheme]);
+    }
 
-  /**
-   * Get app's current color scheme
-   */
-  getScheme(): "light" | "dark" {
-      const scheme =
-      this.currentScheme === "default"
-          ? Appearance.getColorScheme()
-          : this.currentScheme;
-      return scheme ?? "light";
-  }
+    /**
+     * Get app's current color scheme
+     */
+    getScheme(): "light" | "dark" {
+        const scheme =
+            this.currentScheme === "default"
+                ? Appearance?.getColorScheme()
+                : this.currentScheme;
+        return scheme ?? "light";
+    }
 
-  /**
-   * Set color scheme for app
-   * arguments:
-   * scheme - color scheme e.g light/dark/default
-   */
-  setScheme(scheme: SchemeType) {
-      if (!["light", "dark", "default"].includes(scheme)) {
-          throw new Error(
-              `${scheme} is invalid colorScheme, please use 'light' | 'dark' | 'default'`,
-          );
-      }
-      this.currentScheme = scheme;
-      const colorScheme = this.getScheme();
-      Object.assign(this, this.schemes[colorScheme]);
-  }
+    /**
+     * Set color scheme for app
+     * arguments:
+     * scheme - color scheme e.g light/dark/default
+     */
+    setScheme(scheme: SchemeType) {
+        if (!["light", "dark", "default"].includes(scheme)) {
+            throw new Error(
+                `${scheme} is invalid colorScheme, please use 'light' | 'dark' | 'default'`
+            );
+        }
+        this.currentScheme = scheme;
+        const colorScheme = this.getScheme();
+        Object.assign(this, this.schemes[colorScheme]);
+    }
 }
 
 const Scheme = new SchemaClass();
